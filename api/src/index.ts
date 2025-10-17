ee3c13cae415b20dfe71b6b9c897222b6a99fb3a
Carol Gonzaga <dev.carolgonzaga@gmail.com>
2025-10-16 22:00:40 -0300

    fix(api): Adiciona e configura o middleware CORS para permitir requisições do client

--- api/src/index.ts
+++ api/src/index.ts
@@ -1,5 +1,6 @@
 import express from "express";
 import dotenv from "dotenv";
+import cors from "cors";
 import userRoutes from "./routes/user.routes";
 import bookshelfRoutes from "./routes/bookshelf.routes";
 
@@ -8,6 +9,7 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 3001;
 
+app.use(cors());
 app.use(express.json());
 
 // Rotas de Usuário
